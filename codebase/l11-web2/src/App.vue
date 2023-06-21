4e306d92f2653230b4e72e63c2431fbb63cf85de
Veronika Dashuber <veronika.dashuber@qaware.de>
2023-06-21 14:51:57 +0200

    Add lecture sample for Web UI 2

---
+++ codebase/l11-web2/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang='ts'>
+import GreetingComponent from '@/search/GreetingComponent.vue';
+
+function doGreet(greeting: string) {
+  console.log(greeting);
+}
+</script>
+
+<template>
+  <div>
+    <greeting-component :greet-message='`Hallo`'
+                        :placeholder='`Bitte gib Deinen Namen ein`'
+                        @onGreet='doGreet'></greeting-component>
+  </div>
+</template>
+
+<style>
+</style>
+
---
+++ codebase/l11-web2/src/search/GreetingComponent.vue
@@ -0,0 +1,26 @@
+<script setup lang='ts'>
+
+import {ref} from 'vue';
+
+const name = ref('');
+
+const props = defineProps({
+  greetMessage: String,
+  placeholder: String
+});
+
+const emits = defineEmits(['onGreet']);
+
+function doGreet() {
+  emits('onGreet', props.greetMessage + ' ' + name.value);
+}
+</script>
+
+<template>
+  <input v-bind:value='props.greetMessage' disabled>
+  <input type='text' v-model='name' :placeholder='props.placeholder'>
+  <button @click='doGreet'>Greet me!</button>
+</template>
+
+<style>
+</style>
